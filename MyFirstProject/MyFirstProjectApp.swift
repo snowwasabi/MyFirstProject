46c7946ddfabd3bb1bf02783a15ad32e137aa29e
Alexandr Litvinenko <snowwasabi@icloud.com>
2021-10-18 00:49:26 +0600

    init

---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Александр Литвиненко on 18.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
